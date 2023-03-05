b4409126f0bd4899ea94f83a87e9dd61029a45f9
moo-light <binhnguyenthanh19242yahoo@gmail.com>
2023-03-05 18:57:40 +0700

    ElectronicScreen

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
-﻿Console.WriteLine(CodeForceRound277_A(Convert.ToInt64(Console.ReadLine())));
-long CodeForceRound277_A(long n)
+﻿Console.WriteLine(ElectronicScreen("01110110010111110000010101111101"));
+string ElectronicScreen(string s)
 {
-    long s = 0;
-    //for(long i = 1; i <= n; i++)
-    //{
-    //    if (i % 2 == 1) s -= i;
-    //    else s+= i;
-    //    Console.WriteLine(s + " " +i) ;
-    //}
-    //return s;
-    return n % 2 == 0 ? n / 2 : -(n + 1) / 2;
+    Dictionary<string, int> dict = new()
+    {
+        { "00000101", 1 },
+        { "01110110", 2 },
+        { "01110101", 3 },
+        { "00101101", 4 },
+        { "01111001", 5 },
+        { "01111011", 6 },
+        { "01000101", 7 },
+        { "01111111", 8 },
+        { "01111101", 9 },
+        { "01011111", 0 }
+    };
+    string res = "";
+    for (int i = 0; i * 8 < s.Length; i++)
+    {
+        int j = i * 8;
+        res += dict[s.Substring(j,  8)];
+    }
+    return res;
 }
